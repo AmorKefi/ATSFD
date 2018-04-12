077e72220386e1a5a6e8b25774bf7cb551f86765
Amorkefi <kefi.amor@gmail.com>
2018-04-12 12:22:51 +0100

    Commited

--- src/app/containers/sfd-layout/sfd-layout.component.ts
+++ src/app/containers/sfd-layout/sfd-layout.component.ts
@@ -25,7 +25,7 @@ export class SfdLayoutComponent implements OnInit{
       caller :'Ajouter SFD'
     }
     let dialogRef = this.dialog.open(AddSfdDialogComponent, {
-      height: '400px',
+      height: '590px',
       width: '600px',
       data:sfd
     });
@@ -38,7 +38,7 @@ export class SfdLayoutComponent implements OnInit{
   update(sfd){
     sfd.caller="Modifier SFD"
     const dialogConfig = new MatDialogConfig();
-     dialogConfig.height='400px';
+     dialogConfig.height='590px';
      dialogConfig.width='600px';
      dialogConfig.data = sfd;
      this.dialog.open(AddSfdDialogComponent, dialogConfig);
